d71941a0ed2e2d08356bd82b68d4cd7eeaf567e6
imjAY2305 <jagu.jdr121@gmail.com>
2021-08-29 20:39:54 +0530

    master

--- OpenCV/BasicFunctions.py
+++ OpenCV/BasicFunctions.py
@@ -1,7 +1,22 @@
 import cv2 as cv
 
 img = cv.imread("C:/Users/vijay/Desktop/photo.jpg")
+#cv.imshow("Image",img)
 
-cv.imshow("Image",img)
+#Converting to Grayscale
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+#cv.imshow("Gray Image",gray)
+
+#Blur a image
+blur = cv.GaussianBlur(img,(7,7),cv.BORDER_DEFAULT)
+#cv.imshow("BLUR",blur)
+
+#Edge Cascade
+edge = cv.Canny(img,125,175) #We can reduce the number of edges by passing an blured image
+cv.imshow("EDGE",edge)
+
+#Dilating the Image
+dilate = cv.dilate(edge,(7,7),iterations=5)
+cv.imshow("Dilate",dilate)
 cv.waitKey(0)
-cv.destroyAllWindows()
+cv.destroyAllWindows() 
