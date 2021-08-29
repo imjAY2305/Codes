ef451dd345c7f4cc10c760f27daf297f476ddf6c
imjAY2305 <jagu.jdr121@gmail.com>
2021-08-29 11:01:32 +0530

    commit

--- OpenCV/DrawingShapes.py
+++ OpenCV/DrawingShapes.py
@@ -14,8 +14,13 @@ blank[200:300,100:400] = 0,0,255
 #Draw Rectangle
 rect = np.zeros((500,500,3), dtype="uint8")
 rect[:] = 0,255,0 #Green Colour
-cv.rectangle(rect,(20,20),(250,250),(0,0,255),thickness=cv.FILLED)
-cv.imshow("Rectangle",rect) 
+cv.rectangle(rect,(20,20),(250,250),(0,0,255),thickness=-1)
+#cv.imshow("Rectangle",rect)
+
+#Draw Circle
+circle = np.zeros((600,600,3), dtype = "uint8") 
+cv.circle(circle,(300,300),100,(0,0,255),thickness=-1)
+cv.imshow("Circle",circle)
 
 cv.waitKey(20000)
-cv.destroyAllWindows()
+cv.destroyAllWindows() 
